4cd547b5ae6c9a185822ace3b3861541e03d51a2
Aaron Tagliaboschi <jedimastert@gmail.com>
2012-05-21 11:32:11 -0500

    init for project

---
+++ POTmake.pl
@@ -0,0 +1,40 @@
+use potlib;
+
+# POTLIB automation script
+# Written by Aaron Tagliaboschi
+
+# This section parses the arguments
+
+# Check for make or CSV arguments
+$make = 0;
+$csv  = 0;
+my $csvFile, $csvCount;
+foreach(0..1) {
+	if ($ARGV[0] eq '-m') {
+		shift;
+		$make = 1;
+		print "-m \n";
+	} elsif ($ARGV[0] eq '-c') {
+		$csv=1;
+		shift;
+		$cvsFile=shift;
+		$csvCount=shift;
+		print "-c \n";
+	};
+};
+
+# Either starts the CVS parsing or runs makeLib with the arguments
+if ($csv) {
+	%potHash = csvParse($cvsFile);
+	# Gets the headers from the csv
+	@potKeys = keys(%potHash);
+	for ($i=0; $i<$csvCount; $i++) {
+		makeLib(@{$potHash{'LIBFILE'}}[$i], @{$potHash{'NAME'}}[$i], @{$potHash{'LIB'}}[$i], $make);
+	}
+	
+} elsif (@ARGV == 3) {
+	makeLib(shift, shift, shift, $make);
+} else {
+	print "Arguments: @ARGV\n";
+	die "Sorry, wrong number of arguments or something";
+}
---
+++ potlib.pm
@@ -0,0 +1,104 @@
+# The main lib-creating sub
+# arguments:
+#	file: the filename
+#	name: the eventual name of the folder, function, and excecutable
+#	lib:  the name of the lib-folder
+
+sub makeLib {
+	$file = shift;
+	$name = shift;
+	$lib  = shift;
+	$make = shift;
+	print("Now making $name\n");
+	
+	# Copy the particular library and remane it the function name
+	system("cp", $lib, $name, "-r");
+	# And copy the lib source into that
+	system("cp", $file, $name."/src/potlib.f");
+
+	# Now for the fun part...
+	chdir("./".$name);
+	# Text replacement!
+	
+	# First we go through the make file
+	fileFindReplace("Makefile", "NAME", $name);
+	
+	# Then the ML template
+	fileFindReplace("src/pot.tm", "FUNC", $name);
+	
+	# run make if the "-m" was up
+	if ($make) {
+		print "ta da!\n";
+		system("make");
+	}
+	
+	# ...and that's it! Easy, wasn't it? A little too easy...
+	
+	chdir("../");
+}
+
+# Opens a file (fisrt argument) and basically find-and-replaces
+# all instances of the second argument with the third
+sub fileFindReplace {
+	
+	$file = $_[0];
+	$newFile = $_[0]."~";
+	
+	open(FILEIN,  "<", $file);
+	open(FILEOUT, ">", $newFile);
+	
+	while (<FILEIN>) {
+		$replace = $_;
+		$replace =~ s/$_[1]/$_[2]/ge;
+		print FILEOUT $replace;
+	}
+	
+	close FILEIN;
+	close FILEOUT;
+	
+	system("rm", $file);
+	system("mv", $newFile, $file);
+}
+
+# This subroutine parses a CSV file and returns a hash
+
+sub csvParse{
+	# Puts the file line by line in the array @csv
+	open CSVIN, "<", shift;
+	@csv = <CSVIN>;
+	close CSVIN;
+	
+	# Gets the hash keys from the first line
+	@keys = split ',', shift(@csv);
+	
+	# Removes trailing whitespace including \n (gave me a whole heap of trouble)
+	foreach (@keys) {
+		$_ =~ s/^\s+//;
+		$_ =~ s/\s+$//;
+	};
+	
+	# Makes the hash using the keys
+	%csvHash = ();
+	foreach (@keys) {
+		$csvHash{$_} = [];
+	}
+	
+	# Puts the rest of the file in the hash
+	$i=0;
+	foreach (@csv) {
+		@row = split ',', $_;
+		$j=0;
+		foreach (@row) {	
+			$_ =~ s/^\s+//;
+			$_ =~ s/\s+$//;
+			$csvHash{@keys[$j]}[$i]=$_;
+			$j++;
+		}
+		$i++
+	}
+	
+	return %csvHash;
+}
+
+# Return true for the lib-user's sake
+1;
