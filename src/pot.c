026f40555c3ec5954102f3aa12586e4e73e5bc32
Aaron Tagliaboschi <jedimastert@gmail.com>
2012-11-13 11:26:50 -0600

    Now licensed under the GPLv3

--- src/pot.c
+++ src/pot.c
@@ -1,3 +1,21 @@
+/*
+AUTOPot compatibility layer for POTLIB library using Mathlink. Made for compiling with POTLIB library
+Copyright (C) 2012 Aaron Tagliaboschi
+
+The AUTOPot compatibility layer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "mathlink.h"
 #define N3ATOM 75
 #define NATOM 25
--- src/utility.f
+++ src/utility.f
@@ -1,3 +1,18 @@
+!Utility subroutines to be used with POTLIB2001 interface libraries
+!Copyright (C) 2003 R. J. Duchovic, Y. L. Volobuev, G. C. Lynch, A. W. Jasper, D. G. Truhlar, T. C. Allison, A. F. Wagner, B. C. Garrett, J. Espinosa-García, and J. C. Corchado
+
+!These subroutines are free software: you can redistribute it and/or modify
+!it under the terms of the GNU General Public License as published by
+!the Free Software Foundation, either version 3 of the License, or
+!(at your option) any later version.
+
+!This program is distributed in the hope that it will be useful,
+!but WITHOUT ANY WARRANTY; without even the implied warranty of
+!MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+!GNU General Public License for more details.
+
+!You should have received a copy of the GNU General Public License
+!along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
       SUBROUTINE POTINFO
 
