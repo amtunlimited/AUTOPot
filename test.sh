b4a2c5895b0af183751c330d4a7b81c64bb20b69
Aaron Tagliaboschi <jedimastert@gmail.com>
2013-01-08 12:45:17 -0600

    cleanup

--- test.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -e "mathdir" ]; then
-	math -run 'Write["stdout", $InstallationDirectory]; Exit[]' -noprompt > mathdir
-fi
-
-mathdir=`cat mathdir`
-
-mathdir=`echo $mathdir | sed "s/^\"\(.*\)\"$/\1/"`
-
-echo $mathdir
